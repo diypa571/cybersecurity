6bbf4ed8d1b2d16b09fa071fe97fb2623ab896ee
Diyar Parwana <diypa571@gmail.com>
2022-12-29 14:07:16 +0100

    Create 755.sh

---
+++ 755.sh
@@ -0,0 +1,8 @@
+ #Diypa571
+ #script that sets the permissions of all files in the current directory and its subdirectories to 755
+ #!/bin/bash
+
+# Set the permissions of all files in the current directory and its subdirectories to 755
+chmod -R 755 *
+
+echo "Permissions  are sets to 755"
